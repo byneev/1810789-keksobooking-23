ddc4484b999d86feaf5abe18faf1fc8076cbb58b
Byneev <byneev.frontend@gmail.com>
2021-07-06 16:31:12 +0300

    Add photo preview

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import './form/form-reset.js';
 import './form/form-submit.js';
 import './map/map-card-generation.js';
 import './form/form-validation.js';
+import './form/form-preview.js';
 import { addFilterRender } from './map/map-data.js';
 import { initMap } from './map/map.js';
 import { activateForm } from './map/map-condition.js';
