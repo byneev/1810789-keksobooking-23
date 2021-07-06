04a2b18a9b20666f4f9eb692f23e3d0b920ad36c
Byneev <byneev.frontend@gmail.com>
2021-07-06 15:52:47 +0300

    Correct main.js imports

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 import './form/form-reset.js';
+import './form/form-submit.js';
 import './map/map-card-generation.js';
 import './form/form-validation.js';
 import { addFilterRender } from './map/map-data.js';
 import { initMap } from './map/map.js';
 import { activateForm } from './map/map-condition.js';
+import { addValidateNoticeForm } from './form/form-validation.js';
 // import some methods
 
 initMap();
 activateForm();
 addFilterRender();
+addValidateNoticeForm();
