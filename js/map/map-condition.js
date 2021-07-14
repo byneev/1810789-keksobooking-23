f2ec1a1be2942ee0831f0cfa4ccfa7d6aae553af
Byneev <byneev.frontend@gmail.com>
2021-07-14 12:50:32 +0300

    Correct debounce as arrow function, remove assign with expression

--- js/map/map-condition.js
+++ js/map/map-condition.js
@@ -4,13 +4,17 @@ const filterFormItems = filterForm.children;
 const noticeFormItems = noticeForm.children;
 
 const deactivatePage = () => {
-  [...filterFormItems, ...noticeFormItems].forEach((element) => (element.disabled = true));
+  for (const element of [...filterFormItems, ...noticeFormItems]) {
+    element.disabled = true;
+  }
   filterForm.classList.add('map__filters--disabled');
   noticeForm.classList.add('ad-form--disabled');
 };
 
 const activatePage = () => {
-  [...filterFormItems, ...noticeFormItems].forEach((element) => (element.disabled = false));
+  for (const element of [...filterFormItems, ...noticeFormItems]) {
+    element.disabled = false;
+  }
   filterForm.classList.remove('map__filters--disabled');
   noticeForm.classList.remove('ad-form--disabled');
 };
